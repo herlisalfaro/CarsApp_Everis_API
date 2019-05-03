5d8110ac123cc2ce4e2092af7916c2591d56edd5
herli <herli.sanchez.alfaro@everis.com>
2019-05-03 14:04:31 +0200

    no message

--- src/main/java/com/everis/cars/boundary/CarResources.java
+++ src/main/java/com/everis/cars/boundary/CarResources.java
@@ -29,73 +29,56 @@ import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
-
-
-
 @Path("cars")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@OpenAPIDefinition( info = @Info(
-        title = "CarsApp_API",
-        version = "0.0",
-        description = "Car' CRUD Functionality"))
+@OpenAPIDefinition(info = @Info(title = "CarsApp_API", version = "0.0", description = "Car' CRUD Functionality"))
 public class CarResources {
-	
+
 	private final static Logger LOGGER = LogManager.getLogger(CarResources.class);
-	
+
 	@EJB
 	CarService carService;
 
-	@GET	
+	@GET
 	@Operation(description = "Get a list of cars")
-	@ApiResponse(responseCode = "200", description= "Returns List of Cars Available")
+	@ApiResponse(responseCode = "200", description = "Returns List of Cars Available")
 	public Response getCars() {
-		
+
 		LOGGER.info("Retrieving Car's List from Service: ");
 		List<Car> cars = carService.getCars();
 		LOGGER.info("Car's List Retrieved");
 		return Response.ok().entity(cars).build();
-		
+
 	}
 
 	@GET
 	@Path("/{id}")
-	@Operation(description = "Pick a Car by its Id",
-				responses = {
-						@ApiResponse(responseCode = "200",description= "Returns an entity Car by its Id"),
-						@ApiResponse(responseCode = "404", description= "Car with such id doesn't exists")
-				})
+	@Operation(description = "Pick a Car by its Id", responses = {
+			@ApiResponse(responseCode = "200", description = "Returns an entity Car by its Id"),
+			@ApiResponse(responseCode = "404", description = "Car with such id doesn't exists") })
 	@Parameter(description = "Car's Id selected by the user", required = true)
 	public Response getCarById(final @PathParam("id") int id) {
-		
+
 		try {
-			
-			LOGGER.info("Getting Car by its Id: "+ id);
+
+			LOGGER.info("Getting Car by its Id: " + id);
 			return Response.ok().entity(carService.getCarById(id)).build();
 
 		} catch (CarNotFoundException e) {
 			LOGGER.error("Car with id " + id + "Not Found");
-			return Response.status(Status.NOT_FOUND).entity("Car with id "+ id + " not found").build();
+			return Response.status(Status.NOT_FOUND).entity("Car with id " + id + " not found").build();
 		}
 
 	}
 
 	@POST
-	@Operation(
-		    description = "Create new car",
-		    responses = {
-		        @ApiResponse(
-		            responseCode = "200",
-		            description = "Car has been created"
-		        ),
-		        @ApiResponse(
-		            responseCode = "400", 
-		            description = "Could not create a new car"
-		        )
-		     }
-		    
-		)
-	@Parameter(description = "Object Car to be created", required = true ) 
+	@Operation(description = "Create new car", responses = {
+			@ApiResponse(responseCode = "200", description = "Car has been created"),
+			@ApiResponse(responseCode = "400", description = "Could not create a new car") }
+
+	)
+	@Parameter(description = "Object Car to be created", required = true)
 	public Response createCar(final Car car) {
 
 		LOGGER.info("Creating Car: ");
@@ -112,28 +95,16 @@ public class CarResources {
 
 	@PUT
 	@Path("/{id}")
-	@Operation(
-		    description = "Create new car",
-		    responses = {
-		        @ApiResponse(
-		            responseCode = "200",
-		            description = "Car has been created"
-		        ),
-		        @ApiResponse(
-		            responseCode = "400", 
-		            description = "Could not create a new car"
-		        ),
-		        @ApiResponse(
-			            responseCode = "404", 
-			            description = "Car with such id doesn't exists"
-			        )
-		     }
-		    
-		)
-	@Parameter(description = "Car Id to be updated", required = true )
-	@Parameter(description = "Object Car to be updated", required = true )
+	@Operation(description = "Create new car", responses = {
+			@ApiResponse(responseCode = "200", description = "Car has been created"),
+			@ApiResponse(responseCode = "400", description = "Could not create a new car"),
+			@ApiResponse(responseCode = "404", description = "Car with such id doesn't exists") }
+
+	)
+	@Parameter(description = "Car Id to be updated", required = true)
+	@Parameter(description = "Object Car to be updated", required = true)
 	public Response updateCar(final @PathParam("id") int id, final Car car) {
-		
+
 		LOGGER.info("Updating Car: ");
 		final ArrayList<String> validatorsErrors = ValidatorUtil.validate(car);
 		if (validatorsErrors.isEmpty()) {
@@ -143,41 +114,32 @@ public class CarResources {
 				return Response.ok().entity(car).build();
 			} catch (CarNotFoundException e) {
 				LOGGER.error("Car " + car + "with id: " + id + "Not Found");
-				return Response.status(Status.NOT_FOUND).entity("Car with id "+ id +" not found").build();
+				return Response.status(Status.NOT_FOUND).entity("Car with id " + id + " not found").build();
 			}
 
 		} else {
-			LOGGER.error("Failed to update car with id "+ id);
+			LOGGER.error("Failed to update car with id " + id);
 			return Response.status(Status.BAD_REQUEST).entity(validatorsErrors).build();
 		}
 	}
 
 	@DELETE
 	@Path("/{id}")
-	@Operation(
-		    description = "Delete existing car",
-		    responses = {
-		        @ApiResponse(
-		            responseCode = "200",
-		            description = "Car has been deleted"
-		        ),
-		        @ApiResponse(
-		            responseCode = "404", 
-		            description = "Car with such id doesn't exists"
-		        )
-		     }
-		    
-		)
-	@Parameter(description = "Car's Id selected by the user", required = true ) 
+	@Operation(description = "Delete existing car", responses = {
+			@ApiResponse(responseCode = "200", description = "Car has been deleted"),
+			@ApiResponse(responseCode = "404", description = "Car with such id doesn't exists") }
+
+	)
+	@Parameter(description = "Car's Id selected by the user", required = true)
 	public Response deleteCar(final @PathParam("id") int id) {
-		
+
 		try {
 			carService.deleteCar(id);
 			LOGGER.info("Car with id: " + id + "Deleted");
 			return Response.ok().entity("Car Deleted Successfully").build();
 		} catch (CarNotFoundException e) {
 			LOGGER.error("Failed to delete Car with id: " + id);
-			return Response.status(Status.NOT_FOUND).entity("Car with id "+ id +" not found").build();
+			return Response.status(Status.NOT_FOUND).entity("Car with id " + id + " not found").build();
 		}
 	}
 }
